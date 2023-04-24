aecf1c47173dd5e7c78f234f1a820015dfde072c
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-04-24 19:24:02 +0300

    validator added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// GET api/users
-router.get('/', (req, res) => res.send('User route'));
+// POST api/users
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Enter a valid email address').isEmail(),
+    check('password', 'Enter a password(min. 6 characters').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  },
+);
 
 module.exports = router;
