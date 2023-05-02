1d493a689b6219ebe6514aae8e6e87e51df32e02
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-02 23:02:18 +0300

    getting current user profile route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// GET api/profile
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// GET api/profile/me
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar'],
+    );
+
+    if ('profile') {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error.');
+  }
+});
 
 module.exports = router;
