7697cd5a5c83e202fed7930f926a1172464d8d6b
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-03 23:04:25 +0300

    delete profile&user route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -128,4 +128,16 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// DELETE api/profile delete profile, user & posts
+router.delete('/', auth, async (req, res) => {
+  try {
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted.' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error.');
+  }
+});
+
 module.exports = router;
