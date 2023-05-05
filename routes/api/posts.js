d58037e0baa95be269faf6252f7aca4223da628f
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-05 23:10:07 +0300

    like post route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -92,4 +92,25 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+// PUT api/posts/like/:id like a post-private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if already liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked.' });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
