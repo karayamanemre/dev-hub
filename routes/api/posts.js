554df7260588f73c538105361885aca156fa5b4a
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-05 23:14:38 +0300

    unlike post route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -104,8 +104,40 @@ router.put('/like/:id', auth, async (req, res) => {
     ) {
       return res.status(400).json({ msg: 'Post already liked.' });
     }
+
     post.likes.unshift({ user: req.user.id });
+
     await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// PUT api/posts/unlike/:id like a post-private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if it's liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post not liked yet.' });
+    }
+
+    // get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
     res.json(post.likes);
   } catch (err) {
     console.error(err.message);
