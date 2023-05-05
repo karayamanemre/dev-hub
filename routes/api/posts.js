93466783b4e89d8f57f0fa528db4bd64a58a155f
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-05 22:48:23 +0300

    all posts route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -36,4 +36,15 @@ router.post(
   },
 );
 
+// GET api/posts get all posts-private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
