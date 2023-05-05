11f704ee4e2b7d3fded16258b4416b91bcbf972d
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-05 23:32:38 +0300

    delete comment route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -178,4 +178,38 @@ router.post(
   },
 );
 
+// DELETE api/posts/comment/:id/:comment_id delet a comment-private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // pull the comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id,
+    );
+
+    // check comment exist
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist.' });
+    }
+    // check the user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized.' });
+    }
+
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
