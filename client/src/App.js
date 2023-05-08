a7d8ee07ddeb77417efdc4640291be7c96673778
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-08 19:44:10 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => (
-  <Fragment>
-    <h1>App</h1>
-  </Fragment>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<Landing />} />
+    </Routes>
+    <section className="container">
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
+    </section>
+  </BrowserRouter>
 );
 
 export default App;
