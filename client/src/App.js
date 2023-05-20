d9abf44192a9ec704f17cf7e62d13a08aedb2e95
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-20 15:09:28 +0300

    load user auth added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -7,25 +7,37 @@ import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-      </Routes>
-      <section className="container">
-        <Alert></Alert>
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/" element={<Landing />} />
         </Routes>
-      </section>
-    </BrowserRouter>
-  </Provider>
-);
+        <section className="container">
+          <Alert></Alert>
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+          </Routes>
+        </section>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 export default App;
