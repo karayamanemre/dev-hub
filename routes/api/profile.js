583b156d0ef0939a0ed19ffcc08d1ea7d81c36b9
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-23 21:21:54 +0300

    profile reducer

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -16,7 +16,7 @@ router.get('/me', auth, async (req, res) => {
       ['name', 'avatar'],
     );
 
-    if ('profile') {
+    if (!profile) {
       return res.status(400).json({ msg: 'There is no profile for this user' });
     }
 
