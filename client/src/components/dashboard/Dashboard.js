7aeeea6d1f4700ded934ec84037eb70d2f1ed998
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-23 21:42:58 +0300

    Update Dashboard.js

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,18 +2,30 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import Spinner from '../layout/Spinner';
 
 const Dashboard = ({
   getCurrentProfile,
-  auth,
+  auth: { user },
   profile: { profile, loading },
 }) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
-  return loading && profile === null ? <Spinner /> : <div>DashDash</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <div>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <FontAwesomeIcon icon={faUser} />
+        Welcome {user && user.name}
+      </p>
+    </div>
+  );
 };
 
 Dashboard.propTypes = {
