8bc9bd81fb394d41b553852ccbcc650c83a013ba
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-23 21:35:27 +0300

    loading spinner

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'App.css';
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="spinner-bounce1"></div>
+      <div className="spinner-bounce2"></div>
+      <div className="spinner-bounce3"></div>
+    </div>
+  );
+};
+
+export default Spinner;
